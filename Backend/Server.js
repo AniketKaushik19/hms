cb220562acb0d8bf577501f8fcbd2e1380d918d0
AniketKaushik19 <aniketsskaushik@gmail.com>
2025-07-29 13:50:26 +0530

    FIxing bug

--- Backend/Server.js
+++ Backend/Server.js
@@ -29,7 +29,7 @@ app.get('/',(req,res)=>{
 })
 // for hosting 
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist && ../admin/dist")))
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
 app.get("*",(req,res)=>{
     res.sendFile(path.join(__dirname,"../frontend" ,"dist" ,"index.html"))
 })
